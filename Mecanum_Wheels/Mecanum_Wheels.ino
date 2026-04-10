dbfead095281ae3942bf39cde3707a7ad8e1ca17
yugyhjuh <68283680+yugyhjuh@users.noreply.github.com>
2026-04-10 23:35:12 +0800

    Update Mecanum_Wheels.ino
    UPDATE of pins

--- Mecanum_Wheels/Mecanum_Wheels.ino
+++ Mecanum_Wheels/Mecanum_Wheels.ino
@@ -1,26 +1,26 @@
+#define ML_ENA 6
 #define ML_IN1 7
 #define ML_IN2 5
 #define ML_IN3 4
 #define ML_IN4 2
-#define ML_ENA 6
 #define ML_ENB 3
 
 #define MR_ENA 11
 #define MR_IN1 13
 #define MR_IN2 12
-#define MR_IN3 8
-#define MR_IN4 1
-#define MR_ENB 9
+#define MR_IN3 A4
+#define MR_IN4 8
+#define MR_ENB 10
 
 #define BTN_LEFT A1
 #define BTN_RIGHT A0
 
 #define SPEED_FRONT_LEFT 255
-#define SPEED_REAR_LEFT 2
+#define SPEED_REAR_LEFT 200
 #define SPEED_FRONT_RIGHT 255
 #define SPEED_REAR_RIGHT 200
 
-#define ACT_ENA 10
+#define ACT_ENA 9
 #define ACT_IN1 A2
 #define ACT_IN2 A3
 
@@ -30,8 +30,6 @@
 #define ACT_SPEED 255
 
 void setup() {
-  Serial.begin(9600);
-
   pinMode(ML_IN1, OUTPUT); pinMode(ML_IN2, OUTPUT);
   pinMode(ML_IN3, OUTPUT); pinMode(ML_IN4, OUTPUT);
   pinMode(ML_ENA, OUTPUT); pinMode(ML_ENB, OUTPUT);
@@ -65,7 +63,7 @@ void driveLeft() {
   digitalWrite(ML_IN1, HIGH); digitalWrite(ML_IN2, LOW);
   digitalWrite(ML_IN3, HIGH); digitalWrite(ML_IN4, LOW);
   digitalWrite(MR_IN1, LOW); digitalWrite(MR_IN2, HIGH);
-  digitalWrite(MR_IN3, LOW); digitalWrite(MR_IN4, HIGH);
+  digitalWrite(MR_IN3, HIGH); digitalWrite(MR_IN4, LOW);
 }
 
 void driveRight() {
@@ -73,7 +71,7 @@ void driveRight() {
   digitalWrite(ML_IN1, LOW); digitalWrite(ML_IN2, HIGH);
   digitalWrite(ML_IN3, LOW); digitalWrite(ML_IN4, HIGH);
   digitalWrite(MR_IN1, HIGH); digitalWrite(MR_IN2, LOW);
-  digitalWrite(MR_IN3, HIGH); digitalWrite(MR_IN4, LOW);
+  digitalWrite(MR_IN3, LOW); digitalWrite(MR_IN4, HIGH);
 }
 
 void stopMotors() {
@@ -111,9 +109,6 @@ void loop() {
   bool leftPressed = digitalRead(BTN_LEFT) == LOW;
   bool rightPressed = digitalRead(BTN_RIGHT) == LOW;
 
-  Serial.print("DOWN (A4): "); Serial.print(digitalRead(BTN_ACT_DOWN));
-  Serial.print("  UP (A5): "); Serial.println(digitalRead(BTN_ACT_UP));
-
   if (leftPressed && !rightPressed) {
     driveLeft();
     lastState = "left";
@@ -127,13 +122,12 @@ void loop() {
 
   bool downPressed = digitalRead(BTN_ACT_DOWN) == LOW;
   bool upPressed = digitalRead(BTN_ACT_UP) == LOW;
+
   if (downPressed && !upPressed) {
-    Serial.println("Down");
     actuatorDown();
   } else if (upPressed && !downPressed) {
-    Serial.println("Up");
     actuatorUp();
-  } else if (!upPressed && !downPressed) {
+  } else {
     actuatorStop();
   }
 
